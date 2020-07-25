0fc4d3ffc480fe0d59274b1f8cead92490c73f81
codavaen <michael.landon@zodin.dev>
2020-07-25 04:28:24 -0500

    turn handling

--- engine.py
+++ engine.py
@@ -14,6 +14,10 @@ class Engine:
         self.player = player
         self.update_fov()
 
+    def handle_enemy_turns(self) -> None:
+        for entity in self.game_map.entities - {self.player}:
+            print(f"The {entity.name} stands idley.")
+
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
@@ -22,7 +26,7 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
-
+            self.handle_enemy_turns()
             self.update_fov()
 
     def update_fov(self) -> None:
