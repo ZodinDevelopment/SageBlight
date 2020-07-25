7dbb21f9d2b1a1e87750b789c597274e3d01d8d8
codavaen <michael.landon@zodin.dev>
2020-07-25 05:52:32 -0500

    log features

--- main.py
+++ main.py
@@ -54,9 +54,12 @@ def main() -> None:
 
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+            engine.event_handler.handle_events(context)
 
-            engine.event_handler.handle_events()
+            
 
             
 if __name__ == '__main__':
